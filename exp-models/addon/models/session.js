10281f8e617012da445beb5608435cac7bf06fa7
Andy Boughton <abought@gmail.com>
2016-02-08 13:50:18 -0500

    Schema and relationship changes

--- exp-models/addon/models/session.js
+++ exp-models/addon/models/session.js
@@ -6,16 +6,21 @@ import DS from 'ember-data';
 import JamModel from '../mixins/jam-model';
 
 export default DS.Model.extend(JamModel, {
-    softwareVersion: DS.attr('string'),
     parameters: DS.attr(),
+    softwareVersion: DS.attr('string'),
     expData: DS.attr(),  // Data is a reserved keyword in ember
     timestamp: DS.attr('date'),  // Should we instead rely on Jam meta fields as stamp?
     permissions: DS.attr(),
 
     history: DS.hasMany('history'),
+
     // JamDB requires two pieces of info to unambiguously identify a record
-    profileID: DS.belongsTo('account'),
-    //profileVersion: DS.Store('string'),  // TODO: safe to always assume newest profile version?
-    experiment: DS.hasMany('experiment'),
+    profile: DS.belongsTo('profile'),
+    profileVersion: DS.attr('string'),  // TODO: safe to always assume newest profile version?
+
+    experiment: DS.belongsTo('experiment'),
     experimentVersion: DS.attr('string'),
+
+
+    // TODO: Can we add computed properties for experiment name/ ID from a relationship?
 });
