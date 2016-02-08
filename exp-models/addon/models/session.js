19fe53ef6f089e81905a43c227a401f4446ace09
Andy Boughton <abought@gmail.com>
2016-02-08 12:56:51 -0500

    Fix broken model definition on sessions

--- exp-models/addon/models/session.js
+++ exp-models/addon/models/session.js
@@ -6,10 +6,10 @@ import DS from 'ember-data';
 import JamModel from '../mixins/jam-model';
 
 export default DS.Model.extend(JamModel, {
-    softwareVersion: DS.Store('string'),
-    parameters: DS.Store(), // TODO: Nested document
-    data: DS.Store(),  // TODO: Nested document
-    timestamp: DS.Store('date'),  // Should we instead rely on Jam meta fields as stamp?
+    softwareVersion: DS.attr('string'),
+    parameters: DS.attr(),
+    expData: DS.attr(),  // Data is a reserved keyword in ember
+    timestamp: DS.attr('date'),  // Should we instead rely on Jam meta fields as stamp?
     permissions: DS.attr(),
 
     history: DS.hasMany('history'),
@@ -17,5 +17,5 @@ export default DS.Model.extend(JamModel, {
     profileID: DS.belongsTo('account'),
     //profileVersion: DS.Store('string'),  // TODO: safe to always assume newest profile version?
     experiment: DS.hasMany('experiment'),
-    experimentVersion: DS.Store('string'),
+    experimentVersion: DS.attr('string'),
 });
