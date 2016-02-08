61f3875440143567f9ffab1f5ce34b11d2299832
Lauren Barker <leb2dg@virginia.edu>
2016-02-08 17:23:47 -0500

    allow full-text search and filtering

--- exp-models/addon/mixins/jam-document-adapter.js
+++ exp-models/addon/mixins/jam-document-adapter.js
@@ -3,12 +3,15 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     findRecordUrlTemplate: '{+host}/{+namespace}/documents{/id}',
     findAllUrlTemplate: '{+host}/{+namespace}/collections{/jamNamespace}.{+collectionId}/documents',
-    queryUrlTemplate: '{+host}/{+namespace}/collections{/jamNamespace}.{+collectionId}/_search',
+    queryUrlTemplate: '{+host}/{+namespace}/collections{/jamNamespace}.{+collectionId}{/search}',
 
     // TODO: Support creation and deletion of records
 
     urlSegments: {
         // Allows serializer to be reused across multiple types
+        search(type, id, snapshot, query) {
+		  return query.q ? '_search' : 'documents';
+		},
         collectionId: (type, id, snapshot, query) => Ember.Inflector.inflector.pluralize(type),
     }
 });
