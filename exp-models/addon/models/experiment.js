69bbd7fe1d91fe1b7ad824e643ca5b616c7b2ab5
Sam Chrisinger <s.chrisinger@gmail.com>
2016-02-12 13:13:27 -0500

    Add extra fields and computeds to experiment model

--- exp-models/addon/models/experiment.js
+++ exp-models/addon/models/experiment.js
@@ -1,6 +1,7 @@
 /*
 Manage data about one or more documents in the experiments collection
  */
+import Ember from 'ember';
 
 import DS from 'ember-data';
 import JamModel from '../mixins/jam-model';
@@ -8,15 +9,26 @@ import JamModel from '../mixins/jam-model';
 export default DS.Model.extend(JamModel, {
     title: DS.attr('string'),
     description: DS.attr('string'),
-    active: DS.attr('string'),
-    beginDate: DS.attr('date'),
-    endDate: DS.attr('date'),
-    lastEdited: DS.attr('date'),
-
-    eligibilityCriteria: DS.attr(),
+    beginDate: DS.attr('date'),	// TODO: ISODate
+    endDate: DS.attr('date'),	// TODO: ISODate
+    lastEdited: DS.attr('date'),	// TODO: ISODate
     structure: DS.attr(),
 
     permissions: DS.attr(),
 
+    active: DS.attr('string'),
+    isActive: Ember.computed('active', function() {
+        return Ember.isEqual(this.get('active'), 'Active');
+    }),
+
+    administrators: DS.hasMany('admin'),
     history: DS.hasMany('history'),
+    sessions: DS.hasMany('session'),
+
+    eligibilityCriteria: DS.attr('string'),
+    eligibilityString: Ember.computed('eligibilityCriteria', function() {
+        var eligibility = this.get('eligibilityCriteria');
+        // TODO
+        return eligibility || "None";
+    })
 });
