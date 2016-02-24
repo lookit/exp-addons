89d4ec24920e40d13171555782a915ffd87349c6
Lauren Barker <leb2dg@virginia.edu>
2016-02-24 09:19:38 -0500

    create exit survey frame'

---
+++ exp-player/addon/components/exp-exit-survey.js
@@ -0,0 +1,26 @@
+import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
+
+export default ExpFrameBaseComponent.extend({
+    type: 'exp-exit-survey',
+    meta: {
+        name: 'ExpExitSurvey',
+        description: 'TODO: a description of this frame goes here.',
+        parameters: {
+            type: 'object',
+            properties: {
+                id: {
+                    type: 'string',
+                    description: 'A unique identifier for this item'
+                }
+                // define additional parameters here
+            },
+            required: ['id']
+        },
+        data: {
+            type: 'object',
+            properties: {
+                // define data structure here
+            }
+        }
+    }
+});
