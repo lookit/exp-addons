d51d3375539b6a24e2a9e3ee111bdd8d7a3de438
Andy Boughton <abought@gmail.com>
2016-02-26 13:38:09 -0500

    Add initial experiment schema parser [#LEI-89]

---
+++ exp-player/tests/unit/utils/parse-experiment-test.js
@@ -0,0 +1,10 @@
+import parseExperiment from '../../../utils/parse-experiment';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | parse experiment');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = parseExperiment();
+  assert.ok(result);
+});
