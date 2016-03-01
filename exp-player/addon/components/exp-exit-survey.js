7f6fbf9adcad9610479eb3893c34aa68f406c556
Lauren Barker <leb2dg@virginia.edu>
2016-03-01 15:46:17 -0500

    remove quotes on schema keys and merge formData

--- exp-player/addon/components/exp-exit-survey.js
+++ exp-player/addon/components/exp-exit-survey.js
@@ -4,69 +4,69 @@ import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
 import layout from '../templates/components/exp-exit-survey';
 
 const defaultSchema = {
-    "schema": {
-        "title":"Post-study survey",
-        "description":"How was your experience?",
-        "type":"object",
-        "properties": {
-            "birthdate": {
-                "title":"Please confirm your child's birthdate: *"
+    schema: {
+        title:"Post-study survey",
+        description:"How was your experience?",
+        type:"object",
+        properties: {
+            birthdate: {
+                title:"Please confirm your child's birthdate: *"
             },
-            "feedback": {
-                "type":"string",
-                "title":"Your feedback:",
+            feedback: {
+                type:"string",
+                title:"Your feedback:",
             },
-            "privacy": {
-                "type": "string",
-                "title": "Select the privacy level for the video of your participation: *",
-                "enum": ['Publicity and educational use (video may be publicity or educational purposes online or in the press in addition to for research purposes)', 'Scientific use only', 'In-lab use only', 'WITHDRAW your data from this session']
+            privacy: {
+                type: "string",
+                title: "Select the privacy level for the video of your participation: *",
+                enum: ['Publicity and educational use (video may be publicity or educational purposes online or in the press in addition to for research purposes)', 'Scientific use only', 'In-lab use only', 'WITHDRAW your data from this session']
             }
         }
     },
-    "options": {
-        "fields": {
-            "birthdate": {
-                "type": "date",
-                "manualEntry": false,
-                "validator": "required-field",
-                "message": "Please provide a complete and valid birthday.",
-                "helper": "We ask again just to check for typos during registration or people accidentally selecting a different child at the start of the study.",
+    options: {
+        fields: {
+            birthdate: {
+                type: "date",
+                manualEntry: false,
+                validator: "required-field",
+                message: "Please provide a complete and valid birthday.",
+                helper: "We ask again just to check for typos during registration or people accidentally selecting a different child at the start of the study.",
             },
-            "feedback": {
-                "type": "textarea",
-                "rows": 5,
-                "cols": 40,
-                "helper": "Do you have any ideas about how to make this study easier or more fun for families? Did you experience any technical challenges?"
+            feedback: {
+                type: "textarea",
+                rows: 5,
+                cols: 40,
+                helper: "Do you have any ideas about how to make this study easier or more fun for families? Did you experience any technical challenges?"
             },
-            "privacy": {
-                "type": "radio",
-                "removeDefaultNone": true,
-                "validator": "required-field",
-                "message": "Please select a privacy level for your video."
+            privacy: {
+                type: "radio",
+                removeDefaultNone: true,
+                validator: "required-field",
+                message: "Please select a privacy level for your video."
             }
         }
     }   
 };
 const emailOptOutSchema = {
-    "schema": {
-        "type":"object",
-        "properties": {
-            "emailOptOut": {
-                "title":"You are currently signed up to recieve email reminders about this study."
+    schema: {
+        type:"object",
+        properties: {
+            emailOptOut: {
+                title:"You are currently signed up to recieve email reminders about this study."
             }
         }
     },
-    "options": {
-        "fields": {
-            "emailOptOut": {
-                "type": "checkbox",
-                "rightLabel": "Opt out"
+    options: {
+        fields: {
+            emailOptOut: {
+                type: "checkbox",
+                rightLabel: "Opt out"
             }
         },
-        "form": {
-            "buttons": {
-                "finish": {
-                    "title": "Back to Lookit home page"
+        form: {
+            buttons: {
+                finish: {
+                    title: "Back to Lookit home page"
                 }
             }
         }
@@ -181,8 +181,9 @@ export default ExpFrameBaseComponent.extend({
                 }
             },
             finish: function() {
-                root.set('additionalFormData', this.getValue());
-                // TODO: save formData and additionalFormData
+                var formData = root.get('formData');
+                Ember.merge(formData, this.getValue());
+                root.set('formData', formData);
                 console.log('Post-study survey complete.')
                 root.actions.next();
             }
