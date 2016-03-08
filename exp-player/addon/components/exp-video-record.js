1edd570e7181ba9b5cab565a7e0a7c157c106085
Chris Seto <chriskseto@gmail.com>
2016-03-08 14:17:28 -0500

    Add the video watching and recording frame

---
+++ exp-player/addon/components/exp-video-record.js
@@ -0,0 +1,131 @@
+import Ember from 'ember';
+import layout from '../templates/components/exp-video-record';
+
+import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
+// import FullScreen from '../mixins/full-screen';
+import MediaReload from '../mixins/media-reload';
+
+
+//TODO Fullsceen issues/functionality
+export default ExpFrameBaseComponent.extend(MediaReload, {
+    layout: layout,
+    blockUI: false,
+    mayProgress: false,
+    // displayFullscreen: false,  // force fullscreen for all uses of this component, always
+    // fullScreenElementId: 'player-video',
+    videoRecorder: Ember.inject.service(),
+
+    didInsertElement() {
+        this.get('videoRecorder').on('onUploadDone', () => {
+            this.send('next', true);
+            this.get('videoRecorder').destroy();
+        });
+
+        this.get('videoRecorder').on('onCamAccess', this.send.bind(this, 'camAccess'));
+
+        this.set('videoId', [
+            this.get('experiment.id'),
+            this.get('id'),
+            this.get('session.id')
+        ].join('-'));
+
+        this.get('videoRecorder').start(this.get('videoId'), this.$('#recorder'), {
+            hidden: true,
+            record: false
+        }).then(() => {
+            if (!this.get('videoRecorder.camAccess')) {
+                this.set('blockUI', true);
+                this.get('videoRecorder').show();
+                Ember.$('body').addClass('modal-open');
+            }
+        });
+    },
+
+    actions: {
+        next(param) {
+            if (param && this.get('mayProgress'))
+                return this.sendAction('next');
+
+            this.set('mayProgress', true);
+        },
+        camAccess(hasAccess) {
+            if (!hasAccess) return;
+            Ember.$('body').removeClass('modal-open');
+            this.set('blockUI', false);
+            this.get('videoRecorder').hide();
+            Ember.run.scheduleOnce('afterRender', this, () => {
+                this.$('video')[0].play();
+            });
+        },
+        timeUpdate(event) {
+            let currentTime = event.target.currentTime;
+
+            let startStamp = this.get('startRecording') >= 0 ?
+                this.get('startRecording') :
+                (this.$('video')[0].duration + this.get('startRecording'));
+
+            if (!this.get('recorded') && !this.get('videoRecorder.recording') && startStamp <= currentTime) {
+                this.set('recorded', true);
+                this.get('videoRecorder').record();
+                return;
+            }
+
+            let stopStamp = this.get('stopRecording') > 0 ?
+                this.get('stopRecording') :
+                (this.$('video')[0].duration + this.get('stopRecording'));
+
+            if (this.get('videoRecorder.recording') && stopStamp <= currentTime) {
+                this.get('videoRecorder').stop();
+            }
+        }
+    },
+
+    meta: {
+        name: 'Video player',
+        description: 'Component that plays a video',
+        parameters: {
+            type: 'object',
+            properties: {
+                autoforwardOnEnd: {  // Generally leave this true, since controls will be hidden for fullscreen videos
+                    type: 'boolean',
+                    description: 'Whether to automatically advance to the next frame when the video is complete',
+                    default: true
+                },
+                autoplay: {
+                    type: 'boolean',
+                    description: 'Whether to autoplay the video on load',
+                    default: true
+                },
+                poster: {
+                    type: 'string',
+                    description: 'A still image to show until the video starts playing',
+                    default: ''
+                },
+                startRecording: {
+                    type: 'integer',
+                    description: 'The in video time, in seconds, to start recording. Negative indexing is supported. Account for up to 1 second of latency. 0 means the start of the video.',
+                    default: 0
+                },
+                stopRecording: {
+                    type: 'integer',
+                    description: 'The in video time, in seconds, to stop recording. Negative indexing is supported. Account for up to 1 second of latency. 0 means the end of the video.',
+                    default: 0
+                },
+                sources: {
+                    type: 'string',
+                    description: 'List of objects specifying video src and type',
+                    default: []
+                }
+            }
+        },
+        data: {
+            type: 'object',
+            properties: {
+                videoId: {
+                    type: 'string'
+                }
+            },
+            required: ['videoId']
+        }
+    }
+});
