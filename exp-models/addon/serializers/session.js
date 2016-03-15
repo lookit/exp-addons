9223b5bf25afab88943b15fa322ce8cbbac8edd9
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-15 12:12:15 -0400

    Remove createdBy modifiedBy from Session

--- exp-models/addon/serializers/session.js
+++ exp-models/addon/serializers/session.js
@@ -4,5 +4,19 @@ import JamSerializer from '../mixins/jam-serializer';
 import JamDocumentSerializer from '../mixins/jam-document-serializer';
 
 export default DS.JSONAPISerializer.extend(JamSerializer, JamDocumentSerializer, {
-    modelName: 'session'
+    attrs: {
+        createdOn: {serialize: false},
+        createdBy: {serialize: false},
+        modifiedOn: {serialize: false},
+        modifiedBy: {serialize: false}
+    },
+
+    modelName: 'session',
+    extractAttributes() {
+        var attributes = this._super(...arguments);
+        // Remove identifying information from the session model
+        delete attributes.createdBy;
+        delete attributes.modifiedBy;
+        return attributes;
+    }
 });
