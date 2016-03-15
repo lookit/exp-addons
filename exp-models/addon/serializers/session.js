f8d14f7ec4956184f414ad6b88c1f75de606389a
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-15 13:30:23 -0400

    Remove extra attrs from session serializer

--- exp-models/addon/serializers/session.js
+++ exp-models/addon/serializers/session.js
@@ -4,13 +4,6 @@ import JamSerializer from '../mixins/jam-serializer';
 import JamDocumentSerializer from '../mixins/jam-document-serializer';
 
 export default DS.JSONAPISerializer.extend(JamSerializer, JamDocumentSerializer, {
-    attrs: {
-        createdOn: {serialize: false},
-        createdBy: {serialize: false},
-        modifiedOn: {serialize: false},
-        modifiedBy: {serialize: false}
-    },
-
     modelName: 'session',
     extractAttributes() {
         var attributes = this._super(...arguments);
