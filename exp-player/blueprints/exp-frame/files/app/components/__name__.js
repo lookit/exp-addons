17371d0cdbdf9ad3b0c3ebd2fba51533802e459c
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-23 13:34:49 -0400

    Better blueprints with pod structure

--- exp-player/blueprints/exp-frame/files/app/components/__name__.js
+++ exp-player/blueprints/exp-frame/files/app/components/__name__.js
@@ -1,26 +1,2 @@
-import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
-
-export default ExpFrameBaseComponent.extend({
-    type: '<%= dasherizedModuleName %>',
-    meta: {
-        name: '<%= classifiedModuleName %>',
-        description: 'TODO: a description of this frame goes here.',
-        parameters: {
-            type: 'object',
-            properties: {
-                id: {
-                    type: 'string',
-                    description: 'A unique identifier for this item'
-                }
-                // define additional parameters here
-            },
-            required: ['id']
-        },
-        data: {
-            type: 'object',
-            properties: {
-                // define data structure here
-            }
-        }
-    }
-});
+import <%= classifiedModuleName %> from 'exp-player/components/<%= dasherizedModuleName %>/component';
+export default <%= classifiedModuleName %>;
