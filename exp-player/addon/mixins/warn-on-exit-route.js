9797a6c29ef0b95468be56fe19b36e29d7b2aaa7
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-05 20:46:35 -0400

    Move warn-on-exit logic to exp-player
    - remove warn-on-exit controller
    - a little misc cleanup

--- exp-player/addon/mixins/warn-on-exit-route.js
+++ exp-player/addon/mixins/warn-on-exit-route.js
@@ -1,47 +1,13 @@
 import Ember from 'ember';
 
-let {
-    $
-} = Ember;
-
 export default Ember.Mixin.create({
-    forceExit: false,
     actions: {
         willTransition(transition) {
-            if (!this.get('forceExit')) {
-                transition.abort();
-                window.location = this.get('router').generate(transition.intent.name);
-            }
-        },
-        willDestroy() {
-            this._super(...arguments);
-            $(window).off('beforeunload');
-        },
-        allowExit() {
-            this.set('forceExit', true);
-            $(window).off('beforeunload');
+	    // This propagates the Ember transition into a new GET request. This
+	    // allows for easier capture of onbeforeunload events without disrupting
+	    // intra-app navigation.
+            transition.abort();
+            window.location = this.get('router').generate(transition.intent.name);
         }
-    },
-    enter() {
-        this._super(...arguments);
-        $(window).on('beforeunload', () => {
-            return `
-If you're sure you'd like to leave this study early
-you can press 'Leave this Page' to do so.
-
-We'd appreciate it if before you do so you fill out a
-very breif exit survey letting us know how we can use
-any video captured during this session. Press 'Stay on
-this Page' and press F1 to be taken immediately to the
-exit survey.
-
-If this was an accident, just press 'Stay on this Page'
-to continue with the study.
-`;
-        });
-    },
-    exit() {
-        this._super(...arguments);
-        $(window).off('beforeunload');
     }
 });
