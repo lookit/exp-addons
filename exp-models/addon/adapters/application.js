5d1cc988189805b4ec2733be0b9d4c17da8cf217
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-30 14:55:15 -0400

    Proxy namespace config through service
    This allows us to select a namespace at login

--- exp-models/addon/adapters/application.js
+++ exp-models/addon/adapters/application.js
@@ -1,17 +1,22 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 import UrlTemplates from 'ember-data-url-templates';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-import config from 'ember-get-config';
-
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, UrlTemplates, {
-    authorizer: `authorizer:${config.JAMDB.authorizer}`,
-
-    host: config.JAMDB.url,
+    namespaceConfig: Ember.inject.service(),
+    authorizer: Ember.computed(function() {
+	return `authorizer:${this.get('namespaceConfig').get('authorizer')}`;
+    }),
+    host: Ember.computed(function() {
+	return this.get('namespaceConfig').get('url');
+    }),
     namespace: 'v1/id',
 
     urlSegments: {  // Make available to all adapters, not just documents. This appears to be extended rather than overwritten by children.
-        namespaceId: () => config.JAMDB.namespace
+        namespaceId: function() {
+	    return this.get('namespaceConfig').get('namespace');
+	}
     }
 });
