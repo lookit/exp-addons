d32dbb414e28c5ac1617ceae79a20faefba456fa
Saman Ehsan <se8ea@virginia.edu>
2016-07-22 13:31:08 -0400

    Fix exp-free-response form
    - Add meta.data.properties
    - Fix remaining count computed properties
    - Fix validation

--- exp-player/addon/components/exp-free-response/component.js
+++ exp-player/addon/components/exp-free-response/component.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
 import layout from './template';
 import { translationMacro as t } from "ember-i18n";
@@ -5,76 +6,53 @@ import {validator, buildValidations} from 'ember-cp-validations';
 
 const MAX_LENGTH = 75;
 
-var generateValidators = function(questions) {
-  var validators = {};
-  var presence = validator('presence', {
-      presence:true,
-      message: 'This field is required'
-  });
-  for (var question in questions) {
-      var isOptional = questions[question].optional;
-      if (!isOptional) {
-        var key = 'questions.' + question + '.value';
-        validators[key] = presence;
-    }
-  }
-  return validators;
-};
-
 function getRemaining(value) {
     var length = 0;
     if (value !== null) {
-      length = value.length;
+        length = value.length;
     }
     return (MAX_LENGTH - length).toString();
 }
 
-const questions = {
-  q1: {
-    label: 'survey.sections.2.questions.11.label',
-    value: null
-  },
-  q2: {
-    label: 'survey.sections.2.questions.12.label',
-    value: null
-  },
-  q3: {
-    label: 'survey.sections.2.questions.13.label',
-    value: null
-  }
-};
+var presence = validator('presence', {
+    presence:true,
+    message: 'This field is required'
+});
 
-const Validations = buildValidations(generateValidators(questions));
+const Validations = buildValidations({
+    q1: presence,
+    q2: presence,
+    q3: presence
+});
 
 export default ExpFrameBaseComponent.extend(Validations, {
     type: 'exp-free-response',
     layout: layout,
     i18n: Ember.inject.service(),
-    questions: questions,
-    diff1: Ember.computed('questions.q1.value', function() {
-    var remaining = getRemaining(this.questions.q1.value);
-    var translationKey = 'number' + remaining;
-    var message = this.get('i18n').t('survey.sections.2.questions.11.characterCount').string;
-    message = message.replace("75", this.get('i18n').t('number75').string);
-    message = message.replace("0", this.get('i18n').t(translationKey).string);
-    return message;
-  }),
-    diff2: Ember.computed('questions.q2.value', function() {
-    var remaining = getRemaining(this.questions.q2.value);
-    var translationKey = 'number' + remaining;
-    var message = this.get('i18n').t('survey.sections.2.questions.12.characterCount').string;
-    message = message.replace("##", this.get('i18n').t('number75').string);
-    message = message.replace("0", this.get('i18n').t(translationKey).string);
-    return message;
-  }),
-    diff3: Ember.computed('questions.q3.value', function() {
-    var remaining = getRemaining(this.questions.q3.value);
-    var translationKey = 'number' + remaining;
-    var message = this.get('i18n').t('survey.sections.2.questions.13.characterCount').string;
-    message = message.replace("##", this.get('i18n').t('number75').string);
-    message = message.replace("0", this.get('i18n').t(translationKey).string);
-    return message;
-  }),
+    diff1: Ember.computed('q1', function() {
+        var remaining = getRemaining(this.get('q1'));
+        var translationKey = 'number' + remaining;
+        var message = this.get('i18n').t('survey.sections.2.questions.11.characterCount').string;
+        message = message.replace("75", this.get('i18n').t('number75').string);
+        message = message.replace("0", this.get('i18n').t(translationKey).string);
+        return message;
+    }),
+    diff2: Ember.computed('q2', function() {
+        var remaining = getRemaining(this.get('q2'));
+        var translationKey = 'number' + remaining;
+        var message = this.get('i18n').t('survey.sections.2.questions.12.characterCount').string;
+        message = message.replace("##", this.get('i18n').t('number75').string);
+        message = message.replace("0", this.get('i18n').t(translationKey).string);
+        return message;
+    }),
+    diff3: Ember.computed('q3', function() {
+        var remaining = getRemaining(this.get('q3'));
+        var translationKey = 'number' + remaining;
+        var message = this.get('i18n').t('survey.sections.2.questions.13.characterCount').string;
+        message = message.replace("##", this.get('i18n').t('number75').string);
+        message = message.replace("0", this.get('i18n').t(translationKey).string);
+        return message;
+    }),
     meta: {
         name: 'ExpFreeResponse',
         description: 'TODO: a description of this frame goes here.',
@@ -87,7 +65,27 @@ export default ExpFrameBaseComponent.extend(Validations, {
         data: {
             type: 'object',
             properties: {
-                // define data structure here
+                q1: {
+                    type: 'string',
+                    default: null
+                },
+                q2: {
+                    type: 'string',
+                    default: null
+                },
+                q3: {
+                    type: 'string',
+                    default: null
+                }
+            }
+        }
+    },
+    actions: {
+        continue() {
+            if (this.get('validations.isValid')) {
+                this.send('next');
+            } else {
+                this.set('showValidation', true);
             }
         }
     }
