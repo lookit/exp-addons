2c535ecabcf678913ededa7946903af5d3bd469a
Sam Chrisinger <s.chrisinger@gmail.com>
2016-08-16 10:50:36 -0400

    Try lower recording quality

--- exp-player/index.js
+++ exp-player/index.js
@@ -76,7 +76,7 @@ var DEFAULT_OPTIONS = {
   //values: URL paths to the audio/video quality profile files
   //default: '' (in which case the qualityurl flash var is being used, it's found in the .html files embedding videorecorder.swf)
   // qualityurl: '',
-  qualityurl: 'audio_video_quality_profiles/640x480x30x90.xml',
+  qualityurl: 'audio_video_quality_profiles/320x240x30x90.xml',
 
   //maxRecordingTime: Number
   //desc: the maximum recording time in seconds. If set to -1, the 'mrt' flash var parameter will be used, this way the maximum recording time can be set through this flash param specifically if needed.
