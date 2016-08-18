4e00d23178afd1056f9782f46f89c8b52d78236b
Saman Ehsan <se8ea@virginia.edu>
2016-08-18 13:24:16 -0400

    Fix indentation

--- exp-models/addon/authenticators/jam-jwt.js
+++ exp-models/addon/authenticators/jam-jwt.js
@@ -11,36 +11,36 @@ const {
 } = Ember;
 
 export default BaseAuthenticator.extend({
-  url: `${config.JAMDB.url}/v1/auth`,
-  restore(data) {
-    let token = JSON.parse(atob(data.token.split('.')[1]));
-    if (token.exp > moment().unix()) {
-      return RSVP.resolve(data);
-    }
-    return RSVP.reject(data);
-  },
-  authenticate(attrs, token) {
-    if (token) {
-      var payload = jwt_decode(token);
-      var accountId = payload.sub.split('-').pop();
-      return new Ember.RSVP.Promise(resolve => {
-        resolve({
-          id: accountId,
-          token: token
-        });
-      });
-    }
-    return $.ajax({
-      method: 'POST',
-      url: this.url,
-      dataType: 'json',
-      contentType: 'application/json',
-      data: JSON.stringify({
-        data: {
-          type: 'users',
-          attributes: attrs
+    url: `${config.JAMDB.url}/v1/auth`,
+    restore(data) {
+        let token = JSON.parse(atob(data.token.split('.')[1]));
+        if (token.exp > moment().unix()) {
+            return RSVP.resolve(data);
         }
-      })
-    }).then(res => res.data.attributes);
-  }
+        return RSVP.reject(data);
+    },
+    authenticate(attrs, token) {
+        if (token) {
+            var payload = jwt_decode(token);
+            var accountId = payload.sub.split('-').pop();
+            return new Ember.RSVP.Promise(resolve => {
+                resolve({
+                  id: accountId,
+                  token: token
+                });
+            });
+        }
+        return $.ajax({
+            method: 'POST',
+            url: this.url,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                data: {
+                    type: 'users',
+                    attributes: attrs
+                }
+            })
+        }).then(res => res.data.attributes);
+    }
 });
