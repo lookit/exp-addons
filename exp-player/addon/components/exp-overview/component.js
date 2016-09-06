2d88fbda8196e4ebc96cf373cb72a4ac0be241d4
Sam Chrisinger <s.chrisinger@gmail.com>
2016-09-06 13:35:03 -0400

    Revert "Move isp-radio-group to ISP"
    This reverts commit 19804f2f10d3aa4229621b518e1201b3ecdc9906.

--- exp-player/addon/components/exp-overview/component.js
+++ exp-player/addon/components/exp-overview/component.js
@@ -1,5 +1,3 @@
-import Ember from 'ember';
-
 import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
 import layout from './template';
 import {validator, buildValidations} from 'ember-cp-validations';
