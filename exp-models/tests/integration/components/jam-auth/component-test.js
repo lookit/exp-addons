5ee1cb949e2674c8cdf6a195c853da88bfef1303
Sam Chrisinger <s.chrisinger@gmail.com>
2016-09-15 10:56:46 -0400

    Fix test syntax

--- exp-models/tests/integration/components/jam-auth/component-test.js
+++ exp-models/tests/integration/components/jam-auth/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('/jam-auth', 'Integration | Component | jam auth', {
+moduleForComponent('jam-auth', 'Integration | Component | jam auth', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
-  this.render(hbs`{{/jam-auth}}`);
+  this.render(hbs`{{jam-auth}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:"
   this.render(hbs`
-    {{#/jam-auth}}
+    {{#jam-auth}}
       template block text
-    {{//jam-auth}}
+    {{/jam-auth}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
