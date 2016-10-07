bc3e5f9896d5f8ab5737568ef1c6229fe48447d8
Andy Boughton <abought@gmail.com>
2016-10-07 10:05:34 -0400

    Minor code reformatting
    [ci skip]

--- exp-models/addon/models/session.js
+++ exp-models/addon/models/session.js
@@ -1,5 +1,5 @@
 /*
-Manage data about one or more documents in the sessions collection
+ Manage data about one or more documents in the sessions collection
  */
 
 import Ember from 'ember';
@@ -31,15 +31,16 @@ export default DS.Model.extend(AnonJamModel, {
     history: DS.hasMany('history'),
 
     getProfile() {
-	let [accountId, ] = this.get('profileId').split('.');
-	return this.store.findRecord('account', accountId).then((account) => account.profileById(this.get('profileId')));
+        let [accountId, ] = this.get('profileId').split('.');
+        return this.store.findRecord('account', accountId).then((account) => account.profileById(this.get('profileId')));
     },
-    anonProfileId: Ember.computed('profileId', function() {
+    anonProfileId: Ember.computed('profileId', function () {
         // For a profile id of format `<acctShortId>.random`, strip off the identifying account ID
         var profileId = this.get('profileId');
-        return profileId.split ? profileId.split('.')[1] : profileId;}),
+        return profileId.split ? profileId.split('.')[1] : profileId;
+    }),
 
-    experiment: Ember.computed('experimentId', function() {
+    experiment: Ember.computed('experimentId', function () {
         var storeId = this.get('experimentId');
         return this.store.findRecord('experiment', storeId);
     })
