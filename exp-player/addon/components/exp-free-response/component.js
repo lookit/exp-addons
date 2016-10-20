4e0f991111d90c366d998a08800bf0317984b3d8
Saman Ehsan <se8ea@virginia.edu>
2016-10-20 16:24:40 -0400

    Add question asking time of situation
    - Add question asking participants approximately
      what time the situation they are describing began
    - Use ember-power-select component for the dropdown
      to achieve the desired styling.

--- exp-player/addon/components/exp-free-response/component.js
+++ exp-player/addon/components/exp-free-response/component.js
@@ -1,3 +1,4 @@
+var $ = Ember.$;
 import Ember from 'ember';
 import ExpFrameBaseComponent from 'exp-player/components/exp-frame-base';
 import layout from './template';
@@ -21,7 +22,8 @@ var presence = validator('presence', {
 const Validations = buildValidations({
     q1: presence,
     q2: presence,
-    q3: presence
+    q3: presence,
+    q4: presence
 });
 
 export default ExpFrameBaseComponent.extend(Validations, {
@@ -46,12 +48,44 @@ export default ExpFrameBaseComponent.extend(Validations, {
         message = message.replace("0", length.toString());
         return message;
     }),
+    times: [
+        {'12h': '12:00 AM', '24h': '0:00'},
+        {'12h': '1:00 AM', '24h': '1:00'},
+        {'12h': '2:00 AM', '24h': '2:00'},
+        {'12h': '3:00 AM', '24h': '3:00'},
+        {'12h': '4:00 AM', '24h': '4:00'},
+        {'12h': '5:00 AM', '24h': '5:00'},
+        {'12h': '6:00 AM', '24h': '6:00'},
+        {'12h': '7:00 AM', '24h': '7:00'},
+        {'12h': '8:00 AM', '24h': '8:00'},
+        {'12h': '9:00 AM', '24h': '9:00'},
+        {'12h': '10:00 AM', '24h': '10:00'},
+        {'12h': '11:00 AM', '24h': '11:00'},
+        {'12h': '12:00 PM', '24h': '12:00'},
+        {'12h': '1:00 PM', '24h': '13:00'},
+        {'12h': '2:00 PM', '24h': '14:00'},
+        {'12h': '3:00 PM', '24h': '15:00'},
+        {'12h': '4:00 PM', '24h': '16:00'},
+        {'12h': '5:00 PM', '24h': '17:00'},
+        {'12h': '6:00 PM', '24h': '18:00'},
+        {'12h': '7:00 PM', '24h': '19:00'},
+        {'12h': '8:00 PM', '24h': '20:00'},
+        {'12h': '9:00 PM', '24h': '21:00'},
+        {'12h': '10:00 PM', '24h': '22:00'},
+        {'12h': '11:00 PM', '24h': '23:00'},
+        {'12h': '12:00 AM', '24h': '24:00'}
+    ],
+    placeholder: Ember.computed(function() {
+       return this.get('i18n').t('global.selectUnselected');
+    }),
+    q4: null,
 
-    responses: Ember.computed('q1', 'q2', 'q3', function() {
+    responses: Ember.computed('q1', 'q2', 'q3', 'q4', function() {
         return {
             q1: this.get('q1'),
             q2: this.get('q2'),
-            q3: this.get('q3')
+            q3: this.get('q3'),
+            q4: this.get('q4')
         };
     }),
 
@@ -85,6 +119,9 @@ export default ExpFrameBaseComponent.extend(Validations, {
                         },
                         q3: {
                             type: 'string'
+                        },
+                        q4: {
+                            type: 'string'
                         }
                     }
                 }
@@ -96,12 +133,13 @@ export default ExpFrameBaseComponent.extend(Validations, {
             if (this.get('allowNext')) {
                 this.send('next');
             }
-        }
+        },
     },
     loadData: function(frameData) {
         var responses = frameData.responses;
         this.set('q1', responses.q1);
         this.set('q2', responses.q2);
         this.set('q3', responses.q3);
+        this.set('q4', responses.q4);
     }
 });
