171ae7c42bc4e19f6afdc9d2875a361a9706a4e1
Saman Ehsan <se8ea@virginia.edu>
2016-10-21 15:22:21 -0400

    Remove frameIndex & surveyPage fields from session model

--- exp-models/addon/models/session.js
+++ exp-models/addon/models/session.js
@@ -22,9 +22,6 @@ export default DS.Model.extend(AnonJamModel, {
     experimentId: DS.attr('string'),
     experimentVersion: DS.attr('string'),  // TODO: Currently this field is not acted on in any way
 
-    frameIndex: DS.attr(),
-    surveyPage: DS.attr(),
-
     completed: DS.attr('boolean'),  // Filter out sessions that were started, but never finished
     earlyExit: DS.attr(),
 
