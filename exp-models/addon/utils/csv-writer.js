1d4b66a98e7af73d29033e7e2f80f2b6444982d6
Barrett K. Harber <barrett.harber@gmail.com>
2016-12-08 12:25:10 -0500

    Better handling of JavaScript data types, Add more tests

--- exp-models/addon/utils/csv-writer.js
+++ exp-models/addon/utils/csv-writer.js
@@ -3,16 +3,16 @@ import Ember from 'ember';
 /**
  * New line string
  *
- * @type {string}
+ * @type {String}
  */
 export const NL = '\r\n';
 
 /**
  * Flatten a nested object into a single level, with dotted paths for keys
  *
- * @param obj {!Object} - The object to flatten
- * @param prefix {string} - The prefix for the keys
- * @returns {Object} - The flattened object
+ * @param {Object} obj The object to flatten
+ * @param {String} [prefix] The prefix for the keys
+ * @return {Object} The flattened object
  */
 function squashPrefix(obj, prefix) {
     let ret = {};
@@ -42,9 +42,10 @@ function squashPrefix(obj, prefix) {
 
 /**
  * Flatten a nested object into a single level, with dotted paths for keys
+ * *Needed for making squashPrefix map-able (only takes first arg)*
  *
- * @param obj {!Object} - The object to flatten
- * @returns {Object} - The flattened object
+ * @param {Object} obj The object to flatten
+ * @return {Object} The flattened object
  */
 export function squash(obj) {
     return squashPrefix(obj);
@@ -53,14 +54,17 @@ export function squash(obj) {
 /**
  * Extracts the unique object keys from an array of objects
  *
- * @param dataArray {Array<Object>} - An array of objects
- * @returns {Array<string>} - The unique keys
+ * @param  {Array<Object>} dataArray An array of objects
+ * @param  {Number} [maxRows] The maximum number of rows to process
+ * @return {Array<String>} The unique keys
  */
-export function uniqueFields(dataArray) {
+export function uniqueFields(dataArray, maxRows) {
+    const arr = maxRows ? dataArray.slice(0, maxRows) : dataArray;
+
     return [
         ...new Set(
             [].concat(
-                ...dataArray.map(Object.keys)
+                ...arr.map(Object.keys)
             )
         )
     ];
@@ -69,25 +73,42 @@ export function uniqueFields(dataArray) {
 /**
  * Makes a value safe for a CSV
  *
- * @param val {boolean|null|number|undefined|string} - The value to serialize
- * @returns {string}
+ * @param {Boolean|Null|Number|Object|Array|String} val The value to serialize
+ * @return {String}
  */
 function csvSafe(val) {
-    const value = JSON.stringify(val);
+    let value = val;
+
+    switch (typeof value) {
+        case 'boolean':
+            return value ? 'TRUE' : 'FALSE';
+        case 'number':
+            return value.toString();
+        case 'object':
+            if (value === null) {
+                return 'null';
+            }
+
+            value = JSON.stringify(value);
+            /* falls through */
+        default:
+            value = JSON.stringify(value);
+    }
+
     return value ? value.replace(/\\"/g, '""') : '';
 }
 
 /**
  * Converts an array of objects to a standard CSV file
  *
- * @param dataArray {!Array<Object>} - The rows of the CSV
- * @param fields {!Array<string>} - The fields of the CSV array. If not supplied, the the headers are calculated based on the
- * first row of data.
- * @param columnSeparator {string} - A custom column separator
- * @returns {string}
+ * @param {Array<Object>} dataArray The rows of the CSV
+ * @param {Array<String>} fields The fields of the CSV array. If not supplied, the the headers are calculated based on
+ * the first row of data.
+ * @param {String} [columnSeparator=','] A custom column separator
+ * @return {String}
  */
 export function writeCSV(dataArray, fields, columnSeparator=',') {
-    const csv = [fields.join()];
+    const csv = [fields.join(columnSeparator)];
 
     for (const item of dataArray) {
         const line = [];
--- exp-models/tests/unit/utils/csv-writer-test.js
+++ exp-models/tests/unit/utils/csv-writer-test.js
@@ -158,3 +158,163 @@ test('All together', function(assert) {
         `a,mic.check${NL}"one","two"${NL}"one","two"`
     );
 });
+
+test('Varied fields', function(assert) {
+    const data = [
+        {
+            alpha: one,
+            bravo: {
+                charlie: one
+            }
+        },
+        {
+            alpha: two,
+            delta: {
+                echo: two
+            }
+        },
+        {
+            alpha: three,
+            foxtrot: three,
+            golf: three
+        }
+    ];
+
+    const squashedData = data.map(squash);
+    const fields = uniqueFields(squashedData);
+    const result = writeCSV(squashedData, fields);
+
+    assert.strictEqual(
+        result,
+        [
+            'alpha,bravo.charlie,delta.echo,foxtrot,golf',
+            '"one","one",,,',
+            '"two",,"two",,',
+            '"three",,,"three","three"'
+        ].join(NL)
+    );
+});
+
+test('Columns with no data', function(assert) {
+    const fields = [
+        'alpha',
+        'bravo',
+        'yankee',
+        'zulu',
+        'charlie'
+    ];
+
+    const data = [
+        {
+            alpha: one,
+            bravo: one,
+            charlie: one
+        },
+        {
+            alpha: two,
+            bravo: two,
+            charlie: two
+        },
+        {
+            alpha: three,
+            bravo: three,
+            charlie: three
+        }
+    ];
+
+    const squashedData = data.map(squash);
+    const result = writeCSV(squashedData, fields);
+
+    assert.strictEqual(
+        result,
+        [
+            'alpha,bravo,yankee,zulu,charlie',
+            '"one","one",,,"one"',
+            '"two","two",,,"two"',
+            '"three","three",,,"three"'
+        ].join(NL)
+    );
+});
+
+
+
+test('Multiple types of data', function(assert) {
+    const fields = [
+        'alpha',
+        'bravo',
+        'charlie',
+        'delta',
+        'echo',
+        'foxtrot',
+        'golf',
+        'hotel',
+        'juliet',
+        'lima',
+        'kilo'
+    ];
+
+    const data = [
+        {
+            alpha: 1,
+            bravo: null,
+            charlie: 'str'
+        },
+        {
+            delta: {obj: 'val'},
+            echo: ['a', 'b', 'c'],
+            foxtrot: true,
+            golf: false,
+        },
+        {
+            hotel: undefined,
+            juliet: +Infinity,
+            lima: -Infinity,
+            kilo: NaN
+        }
+    ];
+
+    const result = writeCSV(data, fields);
+
+    assert.strictEqual(
+        result,
+        [
+            'alpha,bravo,charlie,delta,echo,foxtrot,golf,hotel,juliet,lima,kilo',
+            '1,null,"str",,,,,,,,',
+            ',,,"{""obj"":""val""}","[""a"",""b"",""c""]",TRUE,FALSE,,,,',
+            ',,,,,,,,Infinity,-Infinity,NaN'
+        ].join(NL)
+    );
+});
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+test('50k records', function(assert) {
+    const data = [];
+    const chars = 'abcdefghijklmnopqrstuvwxyz123456789';
+    const charsMax = chars.length - 1;
+
+    for (let i = 0; i < 50e3; i++) {
+        const obj = data[i] = {};
+
+        for (let j = 0; j < 100; j++) {
+            const length = getRandomInt(1, 5);
+            let key = '';
+
+            for (let k = 0; k < length; k++) {
+                key += chars[getRandomInt(0, charsMax)];
+            }
+
+            obj[key] = 'value';
+        }
+    }
+
+    console.time('Unique Fields');
+    uniqueFields(data);
+    console.timeEnd('Unique Fields');
+
+    assert.ok(true);
+});
