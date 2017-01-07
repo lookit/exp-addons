a4c4bcd7f87663bb1aa2b836c22e5de1801aa621
Andy Boughton <abought@gmail.com>
2017-01-06 22:11:17 -0500

    If RTL, show radio buttons (labels+values) in reverse order
    [#LEI-348]
    [#LEI-344]

--- exp-player/addon/components/isp-radio-group/component.js
+++ exp-player/addon/components/isp-radio-group/component.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import layout from './template';
 
 import ExpRadioGroupComponent from '../../components/radio-group/component';
@@ -5,11 +6,35 @@ import ExpRadioGroupComponent from '../../components/radio-group/component';
 export default ExpRadioGroupComponent.extend({
     layout,
     labelTop: null,
+
+    options: null,
     labels: null,
     formatLabel: null,
 
     // In an RTL rendering, draw the radio buttons in reverse order
     isRTL: null,
+
+    optionsItems: Ember.computed('options', 'isRTL', function() {
+        const isRTL = this.get('isRTL');
+        const options = this.get('options') || [];
+
+        if (!isRTL) {
+            return options;
+        } else {
+            return Ember.copy(options, true).reverse();
+        }
+    }),
+
+    labelsItems: Ember.computed('labels', 'isRTL', function() {
+        const isRTL = this.get('isRTL');
+        const labels = this.get('labels') || [];
+
+        if (!isRTL) {
+            return labels;
+        } else {
+            return Ember.copy(labels, true).reverse();
+        }
+    }),
 
     /**
      * Option values that should not be displayed
