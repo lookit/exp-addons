9f8c6f747bc8c64fda4f17a68449d37f1f4c04ac
Andy Boughton <abought@gmail.com>
2017-01-09 13:38:25 -0500

    Simplified RTL implementation
    [#LEI-344]

--- exp-player/addon/components/isp-radio-group/component.js
+++ exp-player/addon/components/isp-radio-group/component.js
@@ -11,31 +11,6 @@ export default ExpRadioGroupComponent.extend({
     labels: null,
     formatLabel: null,
 
-    // In an RTL rendering, draw the radio buttons in reverse order
-    isRTL: null,
-
-    optionsItems: Ember.computed('options', 'isRTL', function() {
-        const isRTL = this.get('isRTL');
-        const options = this.get('options') || [];
-
-        if (!isRTL) {
-            return options;
-        } else {
-            return Ember.copy(options, true).reverse();
-        }
-    }),
-
-    labelsItems: Ember.computed('labels', 'isRTL', function() {
-        const isRTL = this.get('isRTL');
-        const labels = this.get('labels') || [];
-
-        if (!isRTL) {
-            return labels;
-        } else {
-            return Ember.copy(labels, true).reverse();
-        }
-    }),
-
     /**
      * Option values that should not be displayed
      * @property hiddenOptions
