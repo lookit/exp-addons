1d18b4eb64d3784a6250830f5a4c8329094532b5
Kim Scott <kimscott@mit.edu>
2017-03-23 21:18:53 -0400

    skip instead of commenting out randomizer test

--- exp-player/tests/unit/randomizers/random-parameter-set-test.js
+++ exp-player/tests/unit/randomizers/random-parameter-set-test.js
@@ -1,7 +1,7 @@
-import { module } from 'qunit';
+import { module, skip } from 'qunit';
 import test from 'dummy/tests/ember-sinon-qunit/test';
 
-import { getRandomElement, replaceValues } from 'exp-player/randomizers/random-parameter-set';
+import { getRandomElement, replaceValues, randomizer } from 'exp-player/randomizers/random-parameter-set';
 
 module('Unit | Randomizer | random parameter set');
 
@@ -76,84 +76,84 @@ test('Values are replaced at multiple levels of object hierarchy and within arra
 
 
 // Object.assign doesn't work in tests; omitting this for now (but have checked manually)
-// test('Randomizer creates expected frame list', function (assert) {
-//
-//     const frameId = 'frame-id';
-//     const frameConfig = {
-//         commonFrameProperties: {
-//             'kind': 'exp-lookit-experiment-page'
-//         },
-//         frameList: [
-//             {
-//                 'leftImage': 'LEFTIMAGE1',
-//                 'rightImage': 'frog.jpg',
-//                 'size': 'IMAGESIZE'
-//             },
-//             {
-//                 'leftImage': 'LEFTIMAGE2',
-//                 'rightImage': 'frog.jpg'
-//             },
-//             {
-//                 'leftImage': 'LEFTIMAGE3',
-//                 'rightImage': 'giraffe.jpg',
-//                 'size': 'IMAGESIZE',
-//                 'endAudio': 'ENDAUDIO'
-//             },
-//         ],
-//         parameterSets: [
-//             {
-//                 'LEFTIMAGE1': 'toad.jpg',
-//                 'LEFTIMAGE2': 'snake.jpg',
-//                 'LEFTIMAGE3': 'zebra.jpg',
-//                 'IMAGESIZE': 250,
-//                 'ENDAUDIO': 'roar.mp3'
-//             },
-//             {
-//                 'LEFTIMAGE1': 'bunny.jpg',
-//                 'LEFTIMAGE2': 'cat.jpg',
-//                 'LEFTIMAGE3': 'dog.jpg',
-//                 'IMAGESIZE': 300,
-//                 'ENDAUDIO': 'purr.mp3'
-//             },
-//         ],
-//         parameterSetWeights: [1, 0]
-//     };
-//
-//     // just test on regular frame type, don't test resolving choice frames
-//     // within randomizer (although that should eventually also work)
-//     var resolveFrame = function(_, frame) {
-//         return [frame];
-//     };
-//
-//     const expectedResult = [
-//             {
-//                 'id': 'frame-id',
-//                 'kind': 'exp-lookit-experiment-page',
-//                 'leftImage': 'toad.jpg',
-//                 'rightImage': 'frog.jpg',
-//                 'size': 250
-//             },
-//             {
-//                 'id': 'frame-id',
-//                 'kind': 'exp-lookit-experiment-page',
-//                 'leftImage': 'snake.jpg',
-//                 'rightImage': 'frog.jpg'
-//             },
-//             {
-//                 'id': 'frame-id',
-//                 'kind': 'exp-lookit-experiment-page',
-//                 'leftImage': 'zebra.jpg',
-//                 'rightImage': 'giraffe.jpg',
-//                 'size': 250,
-//                 'endAudio': 'roar.mp3'
-//             },
-//         ];
-//
-//     let [actualResult, conds] = randomizer(frameId, frameConfig, [], resolveFrame);
-//
-//     assert.deepEqual(actualResult, expectedResult,
-//         'Randomizer did not create expected frame list'
-//     );
-//
-// });
-//
+skip('Randomizer creates expected frame list', function (assert) {
+
+    const frameId = 'frame-id';
+    const frameConfig = {
+        commonFrameProperties: {
+            'kind': 'exp-lookit-experiment-page'
+        },
+        frameList: [
+            {
+                'leftImage': 'LEFTIMAGE1',
+                'rightImage': 'frog.jpg',
+                'size': 'IMAGESIZE'
+            },
+            {
+                'leftImage': 'LEFTIMAGE2',
+                'rightImage': 'frog.jpg'
+            },
+            {
+                'leftImage': 'LEFTIMAGE3',
+                'rightImage': 'giraffe.jpg',
+                'size': 'IMAGESIZE',
+                'endAudio': 'ENDAUDIO'
+            },
+        ],
+        parameterSets: [
+            {
+                'LEFTIMAGE1': 'toad.jpg',
+                'LEFTIMAGE2': 'snake.jpg',
+                'LEFTIMAGE3': 'zebra.jpg',
+                'IMAGESIZE': 250,
+                'ENDAUDIO': 'roar.mp3'
+            },
+            {
+                'LEFTIMAGE1': 'bunny.jpg',
+                'LEFTIMAGE2': 'cat.jpg',
+                'LEFTIMAGE3': 'dog.jpg',
+                'IMAGESIZE': 300,
+                'ENDAUDIO': 'purr.mp3'
+            },
+        ],
+        parameterSetWeights: [1, 0]
+    };
+
+    // just test on regular frame type, don't test resolving choice frames
+    // within randomizer (although that should eventually also work)
+    var resolveFrame = function(_, frame) {
+        return [frame];
+    };
+
+    const expectedResult = [
+            {
+                'id': 'frame-id',
+                'kind': 'exp-lookit-experiment-page',
+                'leftImage': 'toad.jpg',
+                'rightImage': 'frog.jpg',
+                'size': 250
+            },
+            {
+                'id': 'frame-id',
+                'kind': 'exp-lookit-experiment-page',
+                'leftImage': 'snake.jpg',
+                'rightImage': 'frog.jpg'
+            },
+            {
+                'id': 'frame-id',
+                'kind': 'exp-lookit-experiment-page',
+                'leftImage': 'zebra.jpg',
+                'rightImage': 'giraffe.jpg',
+                'size': 250,
+                'endAudio': 'roar.mp3'
+            },
+        ];
+
+    let [actualResult, ] = randomizer(frameId, frameConfig, [], resolveFrame);
+
+    assert.deepEqual(actualResult, expectedResult,
+        'Randomizer did not create expected frame list'
+    );
+
+});
+
