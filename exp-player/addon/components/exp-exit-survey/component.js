0068965546da3c39ebf3854e6ab3a35ebba108e9
Kim Scott <kimscott@mit.edu>
2017-11-10 17:53:32 -0500

    for physics study exit survey, only count sessions where participants completed at least 6 videos, to line up with our definition of 'completing' session (and to avoid confusion when participant has e.g. refreshed the config page a bunch)

--- exp-player/addon/components/exp-exit-survey/component.js
+++ exp-player/addon/components/exp-exit-survey/component.js
@@ -92,6 +92,7 @@ export default ExpFrameBaseComponent.extend(Validations, FullScreen, {
     },
     today: new Date(),
     section1: true,
+    minVideosToCountSession: 6,
     showWithdrawalConfirmation: false,
     showValidation: false,
     actions: {
@@ -121,23 +122,44 @@ export default ExpFrameBaseComponent.extend(Validations, FullScreen, {
             this.send('next');
         }
     },
-    currentSessionsCompleted: Ember.computed('frameContext', function() {
-        var pastSessions = this.get('frameContext.pastSessions');
-        if (pastSessions) {
-            return pastSessions.get('length') || 1;
-        }
-        return 1;
-    }),
-    currentDaysSessionsCompleted: Ember.computed('frameContext', function() {
-        // Warning, this implementation may be inaccurate
-        // TODO, figure out what the client's expected behavior is here and resolve
-        // https://openscience.atlassian.net/browse/LEI-111
-        var pastSessionDates = this.get('frameContext.pastSessions').map((session) => moment(session.get('createdOn')));
-        var minDate = moment.min(pastSessionDates);
-        var maxDate = moment.max(pastSessionDates);
+    currentSessionStatus: Ember.computed('frameContext', function() {
+        // Count all sessions
+        //var pastSessions = this.get('frameContext.pastSessions');
+        //if (pastSessions) {
+        //    return pastSessions.get('length') || 1;
+        //}
 
-        return maxDate.diff(minDate, 'days') + 1;
+        // Count only sessions with at least minVideos pref-phys-videos frames
+        var expData = this.get('frameContext.pastSessions').map((session) => session.get('expData'));
+        var expDates = this.get('frameContext.pastSessions').map((session) => moment(session.get('createdOn')));
+        var nSessions = 0;
+        var sessionDates = [];
+        for (var iSess = 0; iSess < expData.length; iSess++) {
+            var keys = Object.keys(expData[iSess]);
+            var nVideos = 0;
+            for (let i = 0; i < keys.length; i++) {
+                var frameKeyName = keys[i];
+                if (frameKeyName.indexOf('pref-phys-videos') !== -1) {
+                    nVideos++;
+                }
+            }
+            if (nVideos >= this.get('minVideosToCountSession')) {
+                nSessions++;
+                sessionDates.push(expDates[iSess]);
+            }
+        }
+        var minDate = moment.min(sessionDates);
+        var maxDate = moment.max(sessionDates);
+        return [nSessions, maxDate.diff(minDate, 'days') + 1];
     }),
+    currentSessionsCompleted: Ember.computed('currentSessionStatus', function()    {
+        return this.get('currentSessionStatus')[0];
+    }),
+
+    currentDaysSessionsCompleted: Ember.computed('currentSessionStatus', function() {
+        return this.get('currentSessionStatus')[1];
+    }),
+
     progressValue: Ember.computed('currentSessionsCompleted', 'idealSessionsCompleted', function() {
         return Math.min(100, Math.ceil((this.get('currentSessionsCompleted') / this.get('idealSessionsCompleted')) * 100));
     }),
