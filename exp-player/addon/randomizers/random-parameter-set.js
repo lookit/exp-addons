8ee6177430ca2deaa4983fb52442d370ba8a2281
Kim Scott <kimscott@mit.edu>
2018-11-01 18:02:11 -0400

    add basic selector syntax to random-parameter-set randomizer; still need to test @RAND and @PERM functionality and extend slightly. working for @N and @UNIQ

--- exp-player/addon/randomizers/random-parameter-set.js
+++ exp-player/addon/randomizers/random-parameter-set.js
@@ -156,6 +156,7 @@ import Ember from 'ember';
 */
 
 function getRandomElement(arr, weights) {
+    weights = weights || Array(arr.length).fill(1);
     var totalProb = weights.reduce((a, b) => a + b, 0);
     var randPos = Math.random() * totalProb;
 
@@ -168,20 +169,21 @@ function getRandomElement(arr, weights) {
     }
 }
 
-function replaceValues(obj, rep) {
-    for (var property in obj) {
-        if (obj.hasOwnProperty(property)) {
-            if (typeof obj[property] === 'object') {
-                obj[property] = replaceValues(obj[property], rep);
-            } else {
-                if (rep.hasOwnProperty(obj[property])) {
-                    obj[property] = rep[obj[property]];
-                }
-            }
-        }
+// http://stackoverflow.com/a/12646864
+function shuffleArray(array) {
+    var shuffled = Ember.$.extend(true, [], array); // deep copy array
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = temp;
     }
-    return obj;
+    return shuffled;
 }
+
+// TODO: in the future we may want to allow nesting of the list-object-selector syntax,
+// e.g. LISTVAR__3__4, LISTVAR1__LISTVAR2__3, LISTVAR1__1__LISTVAR2__3 - but this quickly
+// also requires appropriate processing of parentheses/order-of-operations.
 
 var randomizer = function(frameId, frameConfig, pastSessions, resolveFrame) {
 
@@ -301,6 +303,60 @@ var randomizer = function(frameId, frameConfig, pastSessions, resolveFrame) {
      * @property {Number[]} parameterSetWeights
      */
 
+
+
+    function replaceValues(obj, rep) {
+        for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+                if (typeof obj[property] === 'object') {
+                    obj[property] = replaceValues(obj[property], rep);
+                    // TODO: also allow lists
+                } else {
+                    // If rep has this exact property, just sub in that value
+                    if (rep.hasOwnProperty(obj[property])) {
+                        obj[property] = rep[obj[property]];
+                    } else if (typeof obj[property] === "string" && obj[property].includes("#")) { // Also check for selector syntax:
+                    // property of form X__Y, rep has property X, Y is a valid selector.
+                        var segments = obj[property].split("#");
+                        var propName = segments[0];
+                        var selector = segments.slice(1).join("#");
+                        if (rep.hasOwnProperty(propName)) {
+                            var theList = rep[propName];
+                            if (!Array.isArray(theList)) {
+                                throw "Selector syntax used in frame but corresponding value in parameterSet is not a list"
+                            }
+                            if (Ember.$.isNumeric(selector)) {
+                                var index = Math.round(selector);
+                                obj[property] = theList[index];
+                            } else if (selector === "RAND") {
+                                obj[property] = theList[Math.floor(Math.random()*theList.length)];
+                            } else if (selector === "PERM") {
+                                obj[property] = shuffleArray(theList);
+                            } else if (selector === "UNIQ") {
+                                // If no shuffled version & index stored for this property, create
+                                if (!storedProperties.hasOwnProperty(propName)) {
+                                    storedProperties[propName] = {'shuffledArray': shuffleArray(theList), 'index': 0};
+                                }
+                                // Fetch current element from shuffled array
+                                obj[property] = storedProperties[propName]['shuffledArray'][storedProperties[propName]['index']];
+                                // Move to next for next UNIQ element using this property
+                                storedProperties[propName]['index'] = storedProperties[propName]['index'] + 1;
+                                // Loop around to start if needed
+                                if (storedProperties[propName]['index'] == storedProperties[propName]['shuffledArray'].length) {
+                                    storedProperties[propName]['index'] = 0;
+                                }
+                            } else {
+                                throw "Unknown selector after # in parameter specification";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return obj;
+    }
+
+
     // Select a parameter set to use for this trial.
     if (!(frameConfig.hasOwnProperty('parameterSetWeights'))) {
         frameConfig.parameterSetWeights = new Array(frameConfig.parameterSets.length).fill(1);
@@ -309,6 +365,7 @@ var randomizer = function(frameId, frameConfig, pastSessions, resolveFrame) {
     var parameterData = getRandomElement(frameConfig.parameterSets, frameConfig.parameterSetWeights);
     var parameterSetIndex = parameterData[0];
     var parameterSet = parameterData[1];
+    var storedProperties = {}; // any properties we need to permute and keep track of indices within, across frames, when replacing
 
     var frames = [];
     var thisFrame = {};
@@ -351,4 +408,4 @@ var randomizer = function(frameId, frameConfig, pastSessions, resolveFrame) {
 export default randomizer;
 
 // Export helper functions to support unit testing
-export { getRandomElement, replaceValues};
+export { getRandomElement };
