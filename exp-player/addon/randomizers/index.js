c043c13b09efc2e038a822aae06b5d6f1ab206f5
Kim Scott <kimscott@mit.edu>
2018-11-29 11:42:14 -0500

    remove unused randomizers and exp-survey, select-input components

--- exp-player/addon/randomizers/index.js
+++ exp-player/addon/randomizers/index.js
@@ -1,19 +1,11 @@
-import random from './random';
-import shuffle from './shuffle';
-import rotate from './rotate';
 import next from './next';
 import prefphys from './pref-phys';
-import geometry from './geometry';
 import randomParameterSet from './random-parameter-set';
 import permute from './permute';
 
 export default {
-    random: random,
-    shuffle: shuffle,
-    rotate: rotate,
     next: next,
     permute: permute,
     prefphys: prefphys,
-    geometry: geometry,
     'random-parameter-set': randomParameterSet
 };
