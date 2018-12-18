ffa7c7f6e3ccf6578d74b22bf97f594f4ff79987
Kim Scott <kimscott@mit.edu>
2018-12-18 14:52:17 -0500

    allow html entry from JSON doc for now

--- exp-player/addon/helpers/exp-format.js
+++ exp-player/addon/helpers/exp-format.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function expFormat(text /*, hash*/) {
-    text = Ember.Handlebars.Utils.escapeExpression(text);
+    //text = Ember.Handlebars.Utils.escapeExpression(text);
 
     text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
     text = text.replace(/\t/gm, '&nbsp;&nbsp;&nbsp;&nbsp;');
